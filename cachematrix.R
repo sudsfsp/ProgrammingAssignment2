b4f4da4d657dab3f8c58ddfc1e533b049d50536f
Sudhakar <sudhakar.sfsp@gmail.com>
2015-09-23 00:10:07 +0530

    Completed with new codes and Comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,27 +1,30 @@
 ## Create functions that cache the inverse of a matrix.
-## In R, solve () handle only "square" matrix so matrix will 
-## be in square matrix format. 
+##  We can assume the matrix is invertible, and only square
+## matrices are invertible. 
 
-## create argument in makeCacheMatrix function with   
-## the help of matrix() format. 
+## You can assign "square matrix" or "invertible matrix" 
+##  whether in matrix () or c () (vector) format. 
 
 ## Function Example
 
-## create matrix when calling makeCacheMatrix function
+## Create matrix in makeCacheMatrix argument :
 
+## source ("cachematrix.R")
 ## a <- makeCacheMatrix (matrix(2,3,2,4),nrow=2,ncol=2)
+# # OR
+## a <- makeCacheMatrix (c(2,3,2,4))
 
-## cachesolve (a) #cachesolve solves inverse of matrix
+## cacheSolve (a) #cacheSolve compute inverse of matrix
 
 ##        [,1]  [,2]
 ## [1,]  2.0   -1
 ## [2,] -1.5    1
 
-## We verify the answer manually using solve() function
+## We verify the above answer manually using solve() function
 ## solve(matrix(c(2,3,2,4),2,2))
 
 ## makeCacheMatrix function store list of functions too: 
-## set: assign the value of the your new matrix
+## set: assign the value of the new matrix
 ## get: display the value of the matrix
 ## set_InvMatrix: set the value of the inverse matrix
 ## get_Invmatrix: get the value of the inverse matrix
@@ -32,20 +35,27 @@ makeCacheMatrix <- function(x = matrix()) {
                 x <<- y
                 m <<- NULL
         }
-        get <- function() x
-        
-        set_InvMatrix <- function(mat) m <<- mat
-        
+        get <- function() { 
+                ## Calculate length and square root of x for making 
+                ## "Square Matrix" if function argument in vector format
+                len <- length (x)
+                x <- matrix (x, nrow=len/sqrt(len), ncol= len/sqrt(len)) 
+        }
+        set_InvMatrix <- function(mat) {
+                len2 <- length (mat)
+                m <<- matrix (mat, nrow=len2/sqrt(len2), ncol= len2/sqrt(len2))
+        }
         get_InvMatrix <- function() m
-        
-        list(set = set, get = get,
-             set_InvMatrix = set_InvMatrix,
-             get_InvMatrix =  get_InvMatrix)
+        list (set = set, 
+              get = get,
+              set_InvMatrix = set_InvMatrix,
+              get_InvMatrix =  get_InvMatrix
+        )
 }
 
-##  Calculate the inverse of the special "matrix" created
-##  with the abovefunction and should retrieve the
-##  inverse from the cache if already available.
+##  Calculate the inverse of the special "matrix" returned
+##  by the above function and if inverse matrix already 
+##  calculated then cachesolve retrieve from cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
